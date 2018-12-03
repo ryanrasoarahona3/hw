000670dd8aee013cb3f180c4288d35112283264e
Ryan Rasoarahona <ryanrasoarahona@gmail.com>
2018-12-03 13:50:02 +0300

    add compiled

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var chalk = require("chalk");
+function helloWorld() {
+    console.log(chalk.default.red("Hello world !!!"));
+}
+exports.helloWorld = helloWorld;
+//# sourceMappingURL=index.js.map
