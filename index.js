c61acfcbf711fc2894b1f7e2b2ea1597db05eb51
Ryan Rasoarahona <ryanrasoarahona@gmail.com>
2018-12-04 09:19:58 +0300

    Update compiled

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var chalk = require("chalk");
 function helloWorld() {
-    console.log(chalk.default.red("Hello world 😅 !!!"));
+    console.log(chalk.default.red("Hello world 😅😅😅 !!!"));
 }
 exports.helloWorld = helloWorld;
 //# sourceMappingURL=index.js.map
